80b60d03ae832d1a6582f0d0f500a47c03f9a666
Bhargava999 <bhargava.mm111@gmail.com>
2024-01-08 23:11:21 +0530

    addinf files

---
+++ README-template.md
@@ -0,0 +1,65 @@
+# Frontend Mentor - Todo app solution
+
+This is a solution to the [Todo app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/todo-app-Su1_KokOW). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Add new todos to the list
+- Mark todos as complete
+- Delete todos from the list
+- Filter by all/active/complete todos
+- Clear all completed todos
+- Toggle light and dark mode
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- [React](https://reactjs.org/) - JS library
+
+### What I Learned
+
+During the development of this Tip Calculator app, I gained valuable insights and skills:
+
+- **React State Management:** Utilized the `useState` hook to manage and update state variables for bill amount, number of people, selected tip percentage, and custom tip input.
+
+- **Conditional Rendering:** Implemented conditional rendering to display error messages and prevent calculations when input values are not valid.
+
+- **Responsive Design:** Employed mobile-first design principles and utilized Flexbox and CSS Grid to create a responsive layout that adapts to various screen sizes.
+
+- **Form Handling:** Managed form inputs for bill amount, number of people, and custom tip using controlled components, ensuring a smooth user experience.
+
+- **React Component Structure:** Organized the code into functional components, promoting reusability and maintainability.
+
+- **Styled Components:** Leveraged the Styled Components library for styling, enabling a more modular and maintainable CSS approach.
+
+These learnings have enhanced my proficiency in React development, state management, and responsive design, contributing to my overall growth as a front-end developer.
+
+## Author
+
+- Linkdin - [Bhargava Manikanta](https://www.your-site.com)
+- Frontend Mentor - [@Bhargava999](https://www.frontendmentor.io/profile/yourusername)
--- src/App.js
+++ src/App.js
@@ -1,23 +1,217 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import sunIcon from "./images/icon-sun.svg";
+import moonIcon from "./images/icon-moon.svg";
+import selectIcon from "./images/icon-check.svg";
+import CrossIcon from "./images/icon-cross.svg";
+import { useState } from "react";
+
+const todos = [
+  { id: 0, title: "Complete online JavaScript course", completed: true },
+  { id: 1, title: "Jog around the park 3x", completed: false },
+  { id: 2, title: "10 minutes meditation", completed: false },
+  { id: 3, title: "Read for 1 hour", completed: false },
+  { id: 4, title: "Pick up groceries", completed: false },
+  { id: 5, title: "Complete Todo App on FrontEnd Mentor", completed: true },
+];
 
 function App() {
+  const [theme, setTheme] = useState(true);
+  const [newTodos, setNewTodos] = useState(todos);
+
+  function handleTheme() {
+    setTheme((theme) => !theme);
+  }
+
+  function handleTodo(item) {
+    setNewTodos([...newTodos, item]);
+  }
+
+  function handleToggle(id) {
+    setNewTodos(
+      newTodos.map((item) =>
+        item.id === id ? { ...item, completed: !item.completed } : item
+      )
+    );
+  }
+
+  function handleDelete(id) {
+    setNewTodos(newTodos.filter((item) => item.id !== id));
+  }
+
+  function handleClear() {
+    setNewTodos([]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <main className={`app ${theme ? "dark" : "light"}`}>
+      <div className="container">
+        <header className="header">
+          <h1>TODO</h1>
+          {theme ? (
+            <img src={sunIcon} alt="sun" onClick={handleTheme} />
+          ) : (
+            <img
+              src={moonIcon}
+              alt="moon
+            "
+              onClick={handleTheme}
+            />
+          )}
+        </header>
+        <Form onAddTodo={handleTodo} />
+        <List
+          newTodos={newTodos}
+          onToggle={handleToggle}
+          onDelete={handleDelete}
+          onClear={handleClear}
+        />
+      </div>
+      {
+        <div class="attribution">
+          Challenge by{" "}
+          <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+            Frontend Mentor
+          </a>
+          . Coded by{" "}
+          <a href="https://www.linkedin.com/in/bhargava-manikanta-mamidisetti-1022a3222/">
+            Bhargava Manikanta
+          </a>
+          .
+        </div>
+      }
+    </main>
+  );
+}
+
+function Form({ onAddTodo }) {
+  const [description, setDescription] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!description) return;
+    const newTodo = { title: description, id: Date.now, completed: false };
+    onAddTodo(newTodo);
+    setDescription("");
+  }
+  return (
+    <form className="form-controll" onSubmit={handleSubmit}>
+      <button className="circle"></button>
+      <input
+        placeholder="Create a new todo"
+        type="text"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+    </form>
+  );
+}
+
+function List({ newTodos, onToggle, onDelete, onClear }) {
+  const [sortedby, setSortedBy] = useState("all");
+  const itemsleft = newTodos.slice().filter((todo) => !todo.completed).length;
+  let sortedTodos;
+
+  if (sortedby === "all") sortedTodos = newTodos;
+  if (sortedby === "active")
+    sortedTodos = newTodos.slice().filter((todo) => !todo.completed);
+
+  if (sortedby === "completed")
+    sortedTodos = newTodos.slice().filter((todo) => todo.completed);
+  return (
+    <ul className="list">
+      {sortedTodos.map((todo) => (
+        <ListItem
+          todo={todo}
+          onToggle={onToggle}
+          onDelete={onDelete}
+          key={todo.id}
+        />
+      ))}
+      {newTodos.length > 0 && (
+        <ListFooter
+          onClear={onClear}
+          sortedby={sortedby}
+          onSortedBy={setSortedBy}
+          itemsleft={itemsleft}
+        />
+      )}
+    </ul>
+  );
+}
+
+function ListItem({ todo, onToggle, onDelete }) {
+  return (
+    <li className="list-item">
+      <div className="flex-container">
+        <div className="flex-start">
+          {
+            <button
+              className={`circle ${todo.completed ? "checked" : ""}`}
+              onClick={() => onToggle(todo.id)}
+            >
+              {todo.completed && <img src={selectIcon} alt="select" />}
+            </button>
+          }
+
+          <p className={todo.completed ? "strike" : ""}>{todo.title}</p>
+        </div>
+        <div className="flex-end">
+          {todo.completed && (
+            <img
+              src={CrossIcon}
+              alt="cross"
+              className="cross-icon"
+              onClick={() => onDelete(todo.id)}
+            />
+          )}
+        </div>
+      </div>
+    </li>
+  );
+}
+
+function ListFooter({ onClear, sortedBy, onSortedBy, itemsleft }) {
+  return (
+    <div className="list-footer">
+      <span>{itemsleft > 0 ? `${itemsleft} items left` : " "}</span>
+      <Actions sortedBy={sortedBy} onSortedBy={onSortedBy} />
+      <div className="btn" onClick={onClear}>
+        Clear Completed
+      </div>
+    </div>
+  );
+}
+
+function Actions({ sortedBy, onSortedBy }) {
+  return (
+    <div
+      className="category"
+      value={sortedBy}
+      onChange={(e) => onSortedBy(e.target.value)}
+    >
+      <div className="catgory-item">
+        <input type="radio" name="category-type" value="all" id="all" />
+        <label className="category-btn" htmlFor="all">
+          All
+        </label>
+      </div>
+      <div className="catgory-item">
+        <input type="radio" name="category-type" value="active" id="active" />
+        <label className="category-btn" htmlFor="active">
+          Active
+        </label>
+      </div>
+      <div className="catgory-item">
+        <input
+          type="radio"
+          name="category-type"
+          value="completed"
+          id="completed"
+        />
+        <label className="category-btn" htmlFor="completed">
+          Completed
+        </label>
+      </div>
     </div>
   );
 }
